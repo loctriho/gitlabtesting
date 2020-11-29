762a36e9042427cf038cb07104d16dc1969a9f26
loc ho <lochotri@gmail.com>
2020-11-29 22:51:04 +0700

    update

---
+++ src/main/java/com/gitlabexample/gitlab/GitlabApplication.java
@@ -0,0 +1,13 @@
+package com.gitlabexample.gitlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitlabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitlabexample/gitlab/GitlabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitlabexample.gitlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
